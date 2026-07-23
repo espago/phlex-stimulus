dba66baa389fef5cbb2d7452fd41d6ac3a4f2245
Mateusz Drewniak <matmg24@gmail.com>
2026-07-23 13:11:10 +0200

    bump

--- lib/generators/phlex/stimulus/controller/controller_generator.rb
+++ lib/generators/phlex/stimulus/controller/controller_generator.rb
@@ -9,6 +9,7 @@ module Phlex::Stimulus::Generators
 
     source_root File.expand_path('templates', __dir__)
 
+    #: -> void
     def create_files
       template 'controller.ts.erb',
                File.join(destination_root, 'app', 'javascript', 'controllers', class_path, "#{file_name}_controller.ts")
@@ -17,7 +18,20 @@ module Phlex::Stimulus::Generators
                File.join(destination_root, 'app', 'components', class_path, "#{file_name}_controller.rb")
 
       append_to_file File.join(destination_root, 'app', 'javascript', 'controllers', 'index.ts'),
-                     "import \"./#{class_path}/#{file_name}_controller\""
+                     "import \"./#{class_path}/#{file_name}_controller\"\n"
+    end
+
+    private
+
+    #: -> String
+    def path_to_root_controller_dir
+      './' + class_path.split('/').map { '../' } # rubocop:disable Style/StringConcatenation
+                                  .join
+    end
+
+    #: -> String
+    def class_name_without_namespace
+      file_name.camelize
     end
   end
 end

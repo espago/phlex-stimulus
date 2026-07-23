c3dbc83543c080873675a8e11779b66c226970c5
Mateusz Drewniak <matmg24@gmail.com>
2026-07-23 13:12:36 +0200

    bump

--- lib/generators/phlex/stimulus/controller/controller_generator.rb
+++ lib/generators/phlex/stimulus/controller/controller_generator.rb
@@ -25,6 +25,8 @@ module Phlex::Stimulus::Generators
 
     #: -> String
     def path_to_root_controller_dir
+      require 'byebug'
+      byebug
       './' + class_path.split('/').map { '../' } # rubocop:disable Style/StringConcatenation
                                   .join
     end
